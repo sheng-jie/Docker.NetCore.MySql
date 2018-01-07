32e0f805bfa43f36a4262b3d345151921ab93ad2
shengjie_yan <ysjshengjie@live.cn>
2018-01-07 11:51:07 +0800

    添加【Pomelo.EntityFrameworkCore.MySql】NuGet包 添加Product类

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloDocker.Web.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int StockQty { get; set; }
+    }
+}
